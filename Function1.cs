3d827c2ab03a67e4170a770ba2b1a0ceb752f685
Johnson Dubula <jdubula@iqbusiness.net>
2021-10-18 20:43:02 +0200

    Functions

--- Function1.cs
+++ Function1.cs
@@ -14,7 +14,7 @@ namespace FunctionApp45
     {
         [FunctionName("Function1")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
         {
             string name = req.Query["name"];
 
