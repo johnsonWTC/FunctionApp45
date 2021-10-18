8be1a77fdab4a813cc4773dea9b2a38ceb834887
Johnson Dubula <jdubula@iqbusiness.net>
2021-10-18 20:50:12 +0200

    Functions

--- Function1.cs
+++ Function1.cs
@@ -5,15 +5,20 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace FunctionApp45
 {
-    public static class Function1
+    public  class Function1
     {
+        private readonly ICrud _crud;
+        public Function1(ICrud crud)
+        {
+            _crud = crud;
+        }
+
         [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
+        public  async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
         {
             string name = req.Query["name"];
