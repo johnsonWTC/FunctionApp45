b05b44b4d4217521a2cade4f1fb95cf022ed3b7a
Johnson Dubula <jdubula@iqbusiness.net>
2021-10-20 00:03:57 +0200

    Functions

--- UpdateUser.cs
+++ UpdateUser.cs
@@ -16,16 +16,8 @@ namespace FunctionApp45
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "{newUserName}/{id}")] HttpRequest req, string newUserName, int id)
         {
-            string name = req.Query["name"];
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult();
         }
     }
 }
