a0812e967464e9716413af3a598c20b057b9ff86
Johnson Dubula <jdubula@iqbusiness.net>
2021-10-19 23:44:50 +0200

    Functions

--- FindUserByID.cs
+++ FindUserByID.cs
@@ -17,15 +17,7 @@ namespace FunctionApp45
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "FindUserById/{id}")] HttpRequest req, int id)
         {
           
-            string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
         }
